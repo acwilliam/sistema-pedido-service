2038d4770762ad1be06905035084d4deb6dad0b0
William Carvalho <thelordl2@hotmail.com>
2021-06-29 23:16:31 -0300

    Exception Handling - Update

--- src/main/java/com/acwilliammsn/course/services/UserService.java
+++ src/main/java/com/acwilliammsn/course/services/UserService.java
@@ -3,6 +3,8 @@ package com.acwilliammsn.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -45,9 +47,14 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getOne(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
